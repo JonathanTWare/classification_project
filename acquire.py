cc18f0e1ba8a9389a9eb15c8ee771e12686053bc
Jonathan Ware <jonathan.ware02@gmail.com>
2023-06-21 15:31:04 -0500

    edited acquire and prep file to only contain functions for telco data set

--- acquire.py
+++ acquire.py
@@ -4,38 +4,6 @@ from env import get_db_url
 import os
 
 
-
-
-def new_titanic_data():
-    df =  pd.read_sql('SELECT * FROM passengers', get_db_url('titanic_db'))
-    return df
-
-def get_titanic_data():
-    if os.path.isfile('titanic_df.csv'):
-        df = pd.read_csv('titanic_df.csv', index_col = 0)
-
-    else:
-
-        df = new_titanic_data()
-
-        df.to_csv('titanic_df.csv')
-        return df
-
-def new_iris_db():
-    df =  pd.read_sql('SELECT species_id, species_name, sepal_length, sepal_width, petal_length, petal_width FROM measurements JOIN species USING(species_id)', get_db_url('iris_db'))
-    return df
-
-def get_iris_db():
-    if os.path.isfile('iris_df.csv'):
-        df = pd.read_csv('iris_df.csv', index_col = 0)
-
-    else:
-
-        df = new_iris_db()
-
-        df.to_csv('iris_df.csv')
-        return df
-
 def new_telco_data():
    
     conn = get_db_url('telco_churn')
